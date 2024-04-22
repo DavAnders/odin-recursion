f7cb2f49a8d586e5d4a0c9d06ec3ab7526381425
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-21 22:37:29 -0500

    Add basic fib func

--- fib.js
+++ fib.js
@@ -10,6 +10,23 @@ function fibs(num) {
   if (num === 2) {
     return [0, 1];
   }
+  let arr = [0, 1];
+  for (let i = 3; i <= num; i++) {
+    arr.push(arr[i - 2] + arr[i - 3]);
+  }
+  return arr;
+}
+
+function fibsRec(num) {
+  if (num === 0) {
+    return [];
+  }
+  if (num === 1) {
+    return [0];
+  }
+  if (num === 2) {
+    return [0, 1];
+  }
 
   let arr = fibs(num - 1);
   let nextFib = arr[num - 2] + arr[num - 3];
@@ -17,4 +34,4 @@ function fibs(num) {
   return arr;
 }
 
-console.log(fibs(5));
+console.log(fibs(7));
